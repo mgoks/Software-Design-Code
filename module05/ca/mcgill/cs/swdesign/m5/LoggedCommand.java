c1724c7850f088292e9d9417d527042a46f7c505
Halil Murat Goksel <murat.goksel@mail.mcgill.ca>
2017-10-30 12:59:12 -0400

    started LoggedCommand

---
+++ module05/ca/mcgill/cs/swdesign/m5/LoggedCommand.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ca.mcgill.cs.swdesign.m5;
+
+/**
+ * @author Halil Murat
+ *
+ */
+public class LoggedCommand implements Command
+{
+
+	/* (non-Javadoc)
+	 * @see ca.mcgill.cs.swdesign.m5.Command#execute()
+	 */
+	@Override
+	public void execute()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
